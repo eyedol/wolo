8bc96d38582538bf1d3d2650357fda9f0f272327
eyedol <addhen@linuxmail.org>
2010-10-25 03:32:56 +0000

    Added form for adding new contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('wolo.contacts.views',
+urlpatterns = patterns('wolo.contacts.views.views',
 	(r'^$', 'list'),
+    (r'^person/add/$','person.add'),
 )
